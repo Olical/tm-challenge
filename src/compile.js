58e33d34df1a9dfaedfb9b98d53b168daf903998
Oliver Caldwell <olliec87@gmail.com>
2014-11-26 19:44:43 +0000

    Moving towards basic react "Hello, World!"

---
+++ src/compile.js
@@ -0,0 +1,12 @@
+/**
+ * Compiles an array into a React DOM structure recursively. All segments of the node array are optional.
+ *
+ * @param {*[]} node Comprised of a string selector name, params object, string content and child node(s).
+ * @return {Object} A react DOM tree built from your arrays recursively.
+ */
+function compile(node) {
+}
+
+var selectorExpression = /(?=\.)(?=#)/g;
+
+module.exports = compile;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-console.log('Hello, World!');
+var React = require('react');
+
+var container = document.getElementById('main');
+React.renderComponent(React.DOM.p(null, 'Hello, React!'), container);
