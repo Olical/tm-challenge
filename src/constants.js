f36f385fddd3b9a6e760aa3f3519a0f9560971f4
Oliver Caldwell <olliec87@gmail.com>
2014-11-26 22:18:33 +0000

    Very beginnings of fluxxor architecture

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+/**
+ * Used for mapping action names, but can be used for other things too.
+ *
+ * @type {Object}
+ */
+var constants = {
+};
+
+module.exports = constants;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 var React = require('react');
 var compile = require('./compile');
 
-var container = document.getElementById('main');
-React.render(compile(['p', [
-    ['strong', {key: 'bold'}, 'BOLD'],
-    ['em', {key: 'slanty'}, 'SLANTY']
-]]), container);
+/**
+ * Entry point into the application, will load the root component and mount it onto the DOM.
+ */
+function main() {
+    var container = document.getElementById('main');
+    React.render(compile(['p', [
+        ['strong', {key: 'bold'}, 'BOLD'],
+        ['em', {key: 'slanty'}, 'SLANTY']
+    ]]), container);
+}
+
+// GO!
+main();
